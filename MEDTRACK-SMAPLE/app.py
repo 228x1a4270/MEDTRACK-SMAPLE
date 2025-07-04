efe260f53f1b01a3ab15aa66c4c3d8d0a453122c
228x1a4270 <228x1a4270@khitguntur.ac.in>
2025-07-04 11:33:58 +0530

    Update app.py

--- MEDTRACK-SMAPLE/app.py
+++ MEDTRACK-SMAPLE/app.py
@@ -31,7 +31,7 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)
 # ---------------------------------------
 # App Configuration
 # ---------------------------------------
-AWS_REGION_NAME = os.environ.get('AWS_REGION_NAME', 'ap-south-1')
+AWS_REGION_NAME = os.environ.get('AWS_REGION_NAME', 'us-east-1')
 
 # Email Configuration
 SMTP_SERVER = os.environ.get('SMTP_SERVER', 'smtp.gmail.com')
